86418227834e232c9959d7f6c32aad977566b2d2
AlexCorpROS <arcorprintr@gmail.com>
2022-11-14 02:43:38 +0300

    add Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Задайте количество элементов : ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте максимальную длину элементов, которые нужно вывести на экран: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string[] inputArr = getCreatArray(n);
+string[] resultArray = getFindElementsGivenLength(inputArr, m);
+Console.WriteLine($"Массив элементов максимальной длины {m} имеет вид: ");
+Console.WriteLine(string.Join(", ", resultArray));
+
+string[] getCreatArray(int UserNum)
+{
+string[] inputArray = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Задайте {i+1} элемент : ");
+    inputArray[i] = Console.ReadLine();
+}
+Console.WriteLine("Заданный массив элементов имеет вид : "); 
+Console.WriteLine(string.Join(", ", inputArray));
+return inputArray;
+}
+
+string[] getFindElementsGivenLength(string[] inputArray, int UserNum)
+{
+    int count = 0;
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+        if(inputArray[i].Length <= UserNum)
+        count++;
+    }
+    string[] resultArray = new string[count];
+    for (int i = 0, j= 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= UserNum)
+        {
+            resultArray[j] = inputArray[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
